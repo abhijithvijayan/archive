4fc099bfb65a869a074f67a067f9c57b30f24d61
abhijithvijayan <abhijithv@protonmail.com>
2018-10-14 17:52:27 +0530

    navbar shrink

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import './sass/main.scss'
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
 });
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 50) {
+          $('nav').addClass('shrink');
+        } else {
+           $('nav').removeClass('shrink');
+        }
+    });
+});
