bec6a2eb1f8a9374fe3056018801501cc3a2530c
abhijithvijayan <abhijithv@protonmail.com>
2018-10-18 01:29:27 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import 'bootstrap'
 import './sass/main.scss'
 
+/* For tooltip in social icons */
+
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
 });
+
+/* Navbar Fit on scroll */
 
 $(document).ready(function () {
     $(window).scroll(function () {
